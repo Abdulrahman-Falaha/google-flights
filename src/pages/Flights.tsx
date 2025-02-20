33ee99ba30f9e606fb4f3267a7568176ba8ae706
Abdulrahman-Falaha <abodeh.falaha@gmail.com>
2025-02-20 21:44:26 +0300

    project setup

---
+++ src/pages/Flights.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default React.memo(Flights);
+
+function Flights() {
+  return (
+    <div className="flex flex-col w-[40px] gap-10">
+      <div className="text-4xl">gkosdngosdgs</div>
+      <div>zz</div>
+
+      <div className="flex flex-col">
+
+      </div>
+    </div>
+  );
+}
